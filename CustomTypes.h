141113dd2fb90d92384f59c4c3c215eaf4bd06c6
Manan Garg <m_garg@cs.iitr.ac.in>
2026-09-17 18:29:38 +0530

    feat: cache access optimised struct wrapper

--- CustomTypes.h
+++ CustomTypes.h
@@ -13,3 +13,6 @@ struct FreeDeleter
 
 // for use with buffers allocated malloc, calloc, etc.
 using unique_ptr_void = std::unique_ptr<void, FreeDeleter>;
+
+template<class T> requires (sizeof(T) <= 64)
+struct alignas(std::bit_ceil(sizeof(T))) CacheAccessOptimizedStruct : public T {};
